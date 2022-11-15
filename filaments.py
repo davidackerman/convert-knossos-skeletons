e87b5b90a83edeeda4f4094b4d7f2ce492dd954d
David Ackerman <ackermand@janelia.hhmi.org>
2022-11-15 12:51:08 -0500

    add code to calculate bundles

---
+++ filaments.py
@@ -0,0 +1,251 @@
+from functools import cached_property
+from scipy.interpolate import splprep, splev
+import networkx as nx
+from scipy.optimize import fmin
+from typing import List
+from scipy.spatial import distance_matrix
+import numpy as np
+
+
+def add_to_dict(key_value_dict, key, value):
+    if key not in key_value_dict:
+        key_value_dict[key] = np.array([value])
+    else:
+        key_value_dict[key] = np.append(key_value_dict[key], value)
+
+
+class Filaments:
+    def __init__(self, bin_size):
+        self.bin_size = bin_size
+        self.filament_list: List[Filament] = list()
+
+    def add_filament(self, id, node_dic, edge_list):
+        self.filament_list.append(Filament(id, node_dic, edge_list, self.bin_size))
+
+    def combine_filaments_info(self, indices="all"):
+        self.bin_to_cos_theta_dict = {}
+        self.bin_to_R_squared_dict = {}
+        self.bin_to_delta_squared_dict = {}
+        if indices == "all":
+            indices = range(0, len(self.filament_list))
+        for index in indices:
+            current_filament = self.filament_list[index]
+            for key, value in current_filament.bin_to_cos_theta_dict.items():
+                add_to_dict(self.bin_to_cos_theta_dict, key, value)
+            for key, value in current_filament.bin_to_R_squared_dict.items():
+                add_to_dict(self.bin_to_R_squared_dict, key, value)
+            for key, value in current_filament.bin_to_delta_squared_dict.items():
+                add_to_dict(self.bin_to_delta_squared_dict, key, value)
+
+        self.cos_theta = []
+        self.R_squared = []
+        self.delta_squared = []
+        for index in indices:
+            current_filament = self.filament_list[index]
+            self.cos_theta.append(current_filament.cos_theta)
+            self.R_squared.append(current_filament.R_squared)
+            self.delta_squared.append(current_filament.delta_squared)
+
+        self.cos_theta = np.vstack(self.cos_theta)
+        self.R_squared = np.vstack(self.R_squared)
+        self.delta_squared = np.vstack(self.delta_squared)
+
+    @cached_property
+    def point_to_point_info(self):
+        dic = {}
+        for i in range(len(self.filament_list)):
+            filament_i = self.filament_list[i]
+            for j in range(i + 1, len(self.filament_list)):
+                filament_j = self.filament_list[j]
+
+                dist = distance_matrix(filament_i.coords, filament_j.coords)
+                # index and values
+                closest_point_i_per_point_j = np.argmin(dist, axis=0)
+                closest_point_j_per_point_i = np.argmin(dist, axis=1)
+
+                min_distance_per_point_j = np.amin(dist, axis=0)
+                min_distance_per_point_i = np.amin(dist, axis=1)
+
+                abs_cos_theta_per_point_j = np.zeros(closest_point_i_per_point_j.shape)
+                abs_cos_theta_per_point_i = np.zeros(closest_point_j_per_point_i.shape)
+                for point_j, point_i in enumerate(closest_point_i_per_point_j):
+                    cos_theta = np.dot(
+                        filament_i.normalized_tangents[point_i, :],
+                        filament_j.normalized_tangents[point_j, :],
+                    )
+                    abs_cos_theta_per_point_j[point_j] = np.abs(cos_theta)
+                for point_i, point_j in enumerate(closest_point_j_per_point_i):
+                    cos_theta = np.dot(
+                        filament_i.normalized_tangents[point_i, :],
+                        filament_j.normalized_tangents[point_j, :],
+                    )
+                    abs_cos_theta_per_point_i[point_i] = np.abs(cos_theta)
+
+                dic[(i, j)] = (
+                    min_distance_per_point_j,
+                    abs_cos_theta_per_point_j,
+                )
+                dic[(j, i)] = (
+                    min_distance_per_point_i,
+                    abs_cos_theta_per_point_i,
+                )
+
+        return dic
+
+    def calculate_bundles(
+        self, cutoff_distance=20, cutoff_fraction=0.25, cutoff_angle=45
+    ):
+        cutoff_cos_theta = np.abs(np.cos(np.pi * cutoff_angle / 180))
+        self.are_bundled = np.zeros((len(self.filament_list), len(self.filament_list)))
+
+        for i in range(len(self.filament_list)):
+            # coords_filament_i = filament_i.coords
+            for j in range(i + 1, len(self.filament_list)):
+                # coords_filament_j = filament_j.coords
+                # dist = distance_matrix(coords_filament_i, coords_filament_j)
+                # [(i, j)]
+                # # dist is len(coords_i) rows by len(coords_j) columns
+                (
+                    min_distance_per_point_j,
+                    abs_cos_theta_per_point_j,
+                ) = self.point_to_point_info[(i, j)]
+                (
+                    min_distance_per_point_i,
+                    abs_cos_theta_per_point_i,
+                ) = self.point_to_point_info[(j, i)]
+
+                # point_j_touching_point_i_count = 0
+                # cos_theta = 0
+                # for point_j, point_i in enumerate(closest_point_i_per_point_j):
+                #     if min_distance_point_i_per_point_j[point_j] <= cutoff_distance:
+                #         cos_theta = np.dot(
+                #             normalized_tangents_i[point_i, :],
+                #             normalized_tangents_j[point_j, :],
+                #         )
+                #         if np.abs(cos_theta) >= np.arccos(np.pi * cutoff_angle / 180):
+                #             point_j_touching_point_i_count += 1
+
+                # point_i_touching_point_j_count = 0
+                # for point_i, point_j in enumerate(closest_point_j_per_point_i):
+                #     if min_distance_point_j_per_point_i[point_i] <= cutoff_distance:
+                #         cos_theta = np.dot(
+                #             normalized_tangents_i[point_i, :],
+                #             normalized_tangents_j[point_j, :],
+                #         )
+                #         if np.abs(cos_theta) >= np.cos(np.pi * cutoff_angle / 180):
+                #             point_i_touching_point_j_count += 1
+
+                fraction_i = np.mean(
+                    (min_distance_per_point_i <= cutoff_distance)
+                    & (abs_cos_theta_per_point_i >= cutoff_cos_theta)
+                )
+                fraction_j = np.mean(
+                    (min_distance_per_point_j <= cutoff_distance)
+                    & (abs_cos_theta_per_point_j >= cutoff_cos_theta)
+                )
+
+                if fraction_i >= cutoff_fraction:
+                    self.are_bundled[i, j] = 1
+                if fraction_j >= cutoff_fraction:
+                    self.are_bundled[j, i] = 1
+
+
+class Filament:
+    def __init__(self, id, node_dic, edge_list, bin_size):
+        self.id = id
+        self.bin_size = bin_size
+        self.get_coords_tangents_and_tck(node_dic, edge_list)
+        self.get_lengths()
+        self.get_cos_theta()
+        self.get_R_squared()
+        self.get_delta_squared()
+
+    def get_lengths(self):
+        vectors = self.coords[1:, :] - self.coords[:-1, :]
+        lengths = np.sum(vectors**2, axis=1) ** 0.5
+        lengths = np.insert(lengths, 0, 0)
+        self.cumsum_lengths = np.cumsum(lengths)
+
+    def node_edges_to_xyz(self, node_dic, edge_list):
+        g = nx.from_edgelist(edge_list)
+        xyz = []
+        for node in g.nodes():
+            xyz.append(node_dic[node] * 2)  # voxel-size = 2
+        xyz = np.array(xyz)
+        return xyz[:, 0], xyz[:, 1], xyz[:, 2]
+
+    def get_coords_tangents_and_tck(self, node_dic, edge_list):
+        x, y, z = self.node_edges_to_xyz(node_dic, edge_list)
+        k = 3  # default is k=3, but this gave things that shot off to 1E6 sometimes
+        if len(x) <= 3:
+            k = len(x) - 1
+        self.tck, _ = splprep([x, y, z], k=k, s=0)
+
+        unew = np.arange(0, 1.00, 0.01)
+        new_x, new_y, new_z = splev(unew, self.tck)
+        d_x, d_y, d_z = splev(unew, self.tck, der=1)
+        tangents = np.column_stack((d_x, d_y, d_z))
+
+        self.coords = np.column_stack((new_x, new_y, new_z))
+        self.num_points = self.coords.shape[0]
+        self.normalized_tangents = tangents / (
+            np.linalg.norm(tangents, axis=1)[:, None]
+        )
+
+    def get_cos_theta(self):
+        self.bin_to_cos_theta_dict = {}
+        self.cos_theta = np.zeros((np.sum(range(self.num_points)), 2))
+        count = 0
+        for i in range(self.num_points):
+            for j in range(i + 1, self.num_points):
+                l = self.cumsum_lengths[j] - self.cumsum_lengths[i]
+                bin = (l // self.bin_size + 0.5) * self.bin_size
+                cos_theta = np.dot(
+                    self.normalized_tangents[i], self.normalized_tangents[j]
+                )
+                self.cos_theta[count, :] = [l, cos_theta]
+                add_to_dict(self.bin_to_cos_theta_dict, bin, cos_theta)
+                count += 1
+
+    def get_R_squared(self):
+        self.bin_to_R_squared_dict = {}
+        self.R_squared = np.zeros((np.sum(range(self.num_points)), 2))
+        count = 0
+        for i in range(self.num_points):
+            for j in range(i + 1, self.num_points):
+                l = self.cumsum_lengths[j] - self.cumsum_lengths[i]
+                bin = (l // self.bin_size + 0.5) * self.bin_size
+                R_squared = np.linalg.norm(self.coords[i] - self.coords[j]) ** 2
+                self.R_squared[count, :] = [l, R_squared]
+                add_to_dict(self.bin_to_R_squared_dict, bin, R_squared)
+                count += 1
+
+    def get_delta_squared(self):
+        def distance_to_spline(u, tck, point):
+            point_on_spline = np.array(splev(u, tck)).flatten()
+            return np.linalg.norm(point_on_spline - point)
+
+        self.bin_to_delta_squared_dict = {}
+        knots = np.unique(self.tck[0])
+        self.delta_squared = np.zeros((np.sum(range(len(knots))), 2))
+        count = 0
+        for i in range(len(knots)):
+            for j in range(i + 1, len(knots)):
+                # secant midpoint
+                points = np.array(splev([knots[i], knots[j]], self.tck))
+                L = np.linalg.norm(points[1, :] - points[0, :])
+                point = np.array(splev([knots[i], knots[j]], self.tck)).mean(axis=1)
+                u_guess = (knots[i] + knots[j]) / 2
+                res = fmin(
+                    distance_to_spline,
+                    u_guess,
+                    (self.tck, point),
+                    full_output=True,
+                    disp=False,
+                )
+                bin = (L // self.bin_size + 0.5) * self.bin_size
+
+                delta_squared = res[1] ** 2
+                self.delta_squared[count, :] = [L, delta_squared]
+                add_to_dict(self.bin_to_delta_squared_dict, bin, delta_squared)
+                count += 1
---
+++ interpolation.py
@@ -0,0 +1,111 @@
+# taken from https://github.com/nilsec/micron/blob/0f0807d1f45e398ea04e04e7f7cdeafd1a0dab8d/micron/post/analyse_graph.py#L288-L326
+import operator
+
+
+def interpolate_on_grid(z0, y0, x0, z1, y1, x1, voxel_size):
+    """
+    Interpolate a line on a 3D voxel grid.
+    x0, y0, ... (int, physical space e.g. nm):
+    returns: list of voxels forming a line from p0 to p1
+    """
+
+    def dda_round(x):
+        return (x + 0.5).astype(int)
+
+    start = np.array([z0, y0, x0], dtype=float)
+    end = np.array([z1, y1, x1], dtype=float)
+
+    voxel_size = np.array(voxel_size)
+    if np.any(start % voxel_size) or np.any(end % voxel_size):
+        print(start % voxel_size, end % voxel_size)
+        raise ValueError("Start end end position must be multiples of voxel size")
+
+    line = [dda_round(start / voxel_size)]
+
+    if not np.any(start - end):
+        return line
+
+    max_direction, max_length = max(
+        enumerate(abs(end - start)), key=operator.itemgetter(1)
+    )
+
+    dv = (end - start) / max_length
+
+    # We interpolate in physical space to find the shortest distance
+    # linear interpolation but the path is represented in voxels
+    for step in range(int(max_length)):
+        step_point_rescaled = np.array(
+            dda_round(dda_round((step + 1) * dv + start) / voxel_size)
+        )
+        if not np.all(step_point_rescaled == line[-1]):
+            line.append(step_point_rescaled)
+
+    assert np.all(line[-1] == dda_round(end / voxel_size))
+    return line
+
+
+# from /nrs/funke/ecksteinn/nils_data/cosem_data/cosem_runs/full_cell/cosem_hela_2_full/00_data/exports/read_nml.py
+from xml.dom import minidom
+import numpy as np
+
+
+def parse_nml(filename, edge_attribute=None):
+    doc = minidom.parse(filename)
+    annotations = doc.getElementsByTagName("thing")
+
+    annotation_edge_dict = {}
+    node_dic = {}
+    edge_list = []
+    for annotation in annotations:
+        current_edge_list = []
+        nodes = annotation.getElementsByTagName("node")
+        for node in nodes:
+            node_position, node_id = parse_node(node)
+            node_dic[node_id] = node_position
+
+        edges = annotation.getElementsByTagName("edge")
+
+        for edge in edges:
+            (source_id, target_id) = parse_attributes(
+                edge, [["source", int], ["target", int]]
+            )
+            edge_list.append((source_id, target_id))
+            current_edge_list.append((source_id, target_id))
+
+        annotation_id = parse_attributes(annotation, [["id", int]])[0]
+        annotation_edge_dict[annotation_id] = current_edge_list
+
+    return node_dic, edge_list, annotation_edge_dict
+
+
+def parse_node(node):
+    [x, y, z, id_] = parse_attributes(
+        node,
+        [
+            ["x", float],
+            ["y", float],
+            ["z", float],
+            ["id", int],
+        ],
+    )
+
+    point = np.array([z, y, x])
+
+    return point, id_
+
+
+def parse_edge(edge):
+    [source, target] = parse_attributes(edge, [["source", int], ["target", int]])
+
+    return source, target
+
+
+def parse_attributes(xml_elem, parse_input):
+    parse_output = []
+    attributes = xml_elem.attributes
+    for x in parse_input:
+        try:
+            parse_output.append(x[1](attributes[x[0]].value))
+        except KeyError:
+            parse_output.append(None)
+    return parse_output
